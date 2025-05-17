592847fbf21c8e9b1114004935586680697d1bd0
AbelMaireg <abelrighthere@gmail.com>
2025-05-17 21:46:16 +0300

    chore: database and handler initialization encapsulated.

--- handlers/routes.go
+++ handlers/routes.go
@@ -1,11 +1,27 @@
 package handlers
 
 import (
+	"log"
+
+	"app/config"
 	"app/framework"
+	"app/repositories"
 	"app/services"
 )
 
-func SetupRoutes(router *framework.Router, userService services.UserService) {
+func SetupRoutes(router *framework.Router) {
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	db, err := config.NewDB(cfg)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	userRepo := repositories.NewUserRepository(db)
+	userService := services.NewUserService(userRepo)
+
 	RegisterSignUpHandler(userService)
 	RegisterSignInHandler(userService)
 
--- main.go
+++ main.go
@@ -4,31 +4,14 @@ import (
 	"log"
 	"net/http"
 
-	"app/config"
 	"app/framework"
 	"app/handlers"
-	"app/repositories"
-	"app/services"
 )
 
 func main() {
-	cfg, err := config.NewConfig()
-	if err != nil {
-		log.Fatalf("Failed to load config: %v", err)
-	}
-
-	db, err := config.NewDB(cfg)
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-	}
-
-	userRepo := repositories.NewUserRepository(db)
-	userService := services.NewUserService(userRepo)
-
-	router := framework.GetRouter()
-	handlers.SetupRoutes(router, userService)
+	handlers.SetupRoutes(framework.GetRouter())
 
 	addr := ":8080"
 	log.Printf("Server running at http://localhost%s", addr)
-	log.Fatal(http.ListenAndServe(addr, router.Instance))
+	log.Fatal(http.ListenAndServe(addr, framework.GetRouter().Instance))
 }
