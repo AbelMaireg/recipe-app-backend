c748adb5b242416f64f109e8d3929be67523ce09
AbelMaireg <abelrighthere@gmail.com>
2025-05-21 22:55:12 +0300

    build: minio connection enabled.

--- config/config.go
+++ config/config.go
@@ -1,32 +1,109 @@
 package config
 
 import (
+	"context"
+	"fmt"
 	"os"
+	"sync"
 
-	"app/models"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 type Config struct {
 	DatabaseURL string
+	onceDB      sync.Once
 }
 
-func NewConfig() (*Config, error) {
+type MinIO struct {
+	Endpoint  string
+	AccessKey string
+	SecretKey string
+	Bucket    string
+	client    *s3.Client
+	onceMinio sync.Once
+}
+
+func NewConfig() (*Config, *MinIO, error) {
 	dsn := os.Getenv("DATABASE_URL")
 	if dsn == "" {
 		dsn = "host=postgres user=postgres password=secret dbname=userapp port=5432 sslmode=disable"
 	}
-	return &Config{DatabaseURL: dsn}, nil
+
+	minioEndpoint := os.Getenv("MINIO_ENDPOINT")
+	if minioEndpoint == "" {
+		minioEndpoint = "http://minio:9000"
+	}
+	minioAccessKey := os.Getenv("MINIO_ACCESS_KEY")
+	if minioAccessKey == "" {
+		minioAccessKey = "minioadmin"
+	}
+	minioSecretKey := os.Getenv("MINIO_SECRET_KEY")
+	if minioSecretKey == "" {
+		minioSecretKey = "minioadmin"
+	}
+	minioBucket := os.Getenv("MINIO_BUCKET")
+	if minioBucket == "" {
+		minioBucket = "recipe-images"
+	}
+
+	return &Config{DatabaseURL: dsn}, &MinIO{
+		Endpoint:  minioEndpoint,
+		AccessKey: minioAccessKey,
+		SecretKey: minioSecretKey,
+		Bucket:    minioBucket,
+	}, nil
 }
 
 func NewDB(cfg *Config) (*gorm.DB, error) {
-	db, err := gorm.Open(postgres.Open(cfg.DatabaseURL), &gorm.Config{})
+	var db *gorm.DB
+	var err error
+
+	cfg.onceDB.Do(func() {
+		db, err = gorm.Open(postgres.Open(cfg.DatabaseURL), &gorm.Config{})
+	})
 	if err != nil {
 		return nil, err
 	}
-	if err := db.AutoMigrate(&models.User{}); err != nil {
+
+	return db, nil
+}
+
+func (m *MinIO) GetClient() (*s3.Client, error) {
+	var err error
+	m.onceMinio.Do(func() {
+		cfg, errCfg := config.LoadDefaultConfig(context.Background(),
+			config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(m.AccessKey, m.SecretKey, "")),
+			config.WithEndpointResolverWithOptions(
+				aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+					return aws.Endpoint{
+						URL:           m.Endpoint,
+						SigningRegion: "us-east-1",
+						Source:        aws.EndpointSourceCustom,
+					}, nil
+				}),
+			),
+			config.WithRegion("us-east-1"),
+		)
+		if errCfg != nil {
+			err = errCfg
+			return
+		}
+
+		m.client = s3.NewFromConfig(cfg, func(o *s3.Options) {
+			o.UsePathStyle = true // Required for MinIO
+		})
+	})
+
+	if err != nil {
 		return nil, err
 	}
-	return db, nil
+	if m.client == nil {
+		return nil, fmt.Errorf("failed to initialize MinIO client")
+	}
+	return m.client, nil
 }
